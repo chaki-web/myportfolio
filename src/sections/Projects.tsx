1e06bc3bdd8da2b9d89e5b004ed8757c0661d8f8
Janethra Dissanayake <129816926+chaki-web@users.noreply.github.com>
2025-06-25 23:29:33 +0530

    Changed the gap between the cards of the Projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -12,7 +12,7 @@ const portfolioProjects = [
     results: [
       { title: "Exclusively for Sri Lankan Community" },
       { title: "Better Hands on User Experience" },
-      { title: "Paypent Gateways Including Crypto" },
+      { title: "Payment Gateways Including Crypto" },
     ],
     link: "https://youtu.be/4k7IdSLxh6w",
     image: darkSaasLandingPage,
@@ -44,22 +44,21 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>
+  return(
+  <section className="pb-16">
     <div className="container">
       <div className="flex justify-center">
       <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center ">Real-World Results</p></div>
       <h2 className="font-serif text-3xl text-center mt-6">Featured Projects</h2>
       <p className="text-center text-white/60 mt-4">See how I transformed concepts into engaging digital experiences.</p>
-      <div className="flex flex-col mt-10">
+      <div className="flex flex-col mt-10 gap-8">
         {portfolioProjects.map(project=>(
           <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after;content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8">
-            <div className="flex">
               <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase trackin-widest text-sm text-transparent bg-clip-text">
                 <span>{project.company}</span>
                 <span>&bull;</span>
                 <span>{project.year}</span>
               </div>
-            </div>
             <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
             <hr className="border-t-2 border-white/5 mt-4" />
             <ul className="flex flex-col gap-4 mt-4">
@@ -75,13 +74,13 @@ export const ProjectsSection = () => {
                 <span>View in GitHub</span>
                 <ArrowUpRightIcon className="size-4"/>
               </button>
-
             </a>
             <img src={project.image} alt={project.title}
-            className="mt-8" />
+            className="mt-8 -mb-4" />
           </div>
         ))}
       </div>
     </div>
-  </div>;
+  </section>
+  )
 };
