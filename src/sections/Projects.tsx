f7fc6fb2007bf4acadcff0297565caa29e627f8e
Janethra Dissanayake <129816926+chaki-web@users.noreply.github.com>
2025-06-25 22:36:54 +0530

    Updated the Featured Projects Section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,24 +1,26 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg"
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: "Local Freelance Platform",
+    year: "2025",
+    title: "Do.lk",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "Exclusively for Sri Lankan Community" },
+      { title: "Better Hands on User Experience" },
+      { title: "Paypent Gateways Including Crypto" },
     ],
     link: "https://youtu.be/4k7IdSLxh6w",
     image: darkSaasLandingPage,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
+    company: "ACloth",
+    year: "2024",
+    title: "Shopping Management System",
     results: [
       { title: "Boosted sales by 20%" },
       { title: "Expanded customer reach by 35%" },
@@ -28,9 +30,9 @@ const portfolioProjects = [
     image: lightSaasLandingPage,
   },
   {
-    company: "Quantum Dynamics",
+    company: "Luxuary Wearstore",
     year: "2023",
-    title: "AI Startup Landing Page",
+    title: "Auction Management System",
     results: [
       { title: "Enhanced user experience by 40%" },
       { title: "Improved site speed by 50%" },
@@ -50,7 +52,7 @@ export const ProjectsSection = () => {
       <p className="text-center text-white/60 mt-4">See how I transformed concepts into engaging digital experiences.</p>
       <div className="flex flex-col mt-10">
         {portfolioProjects.map(project=>(
-          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after;content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20">
+          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after;content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8">
             <div className="flex">
               <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase trackin-widest text-sm text-transparent bg-clip-text">
                 <span>{project.company}</span>
@@ -58,17 +60,25 @@ export const ProjectsSection = () => {
                 <span>{project.year}</span>
               </div>
             </div>
-            <h3>{project.title}</h3>
-            <hr/>
-            <ul>
+            <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
+            <hr className="border-t-2 border-white/5 mt-4" />
+            <ul className="flex flex-col gap-4 mt-4">
               {project.results.map((result, idx) => (
-                <li key={idx}>{result.title}</li>
+                <li key={idx} className="flex gap-2 text-sm text-white/50">
+                  <CheckCircleIcon className="size-5" />
+                  <span>{result.title}</span>
+                </li>
               ))}
             </ul>
             <a href={project.link}>
-              <button>View Live Site</button>
+              <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                <span>View in GitHub</span>
+                <ArrowUpRightIcon className="size-4"/>
+              </button>
+
             </a>
-            <img src={project.image} alt={project.title} />
+            <img src={project.image} alt={project.title}
+            className="mt-8" />
           </div>
         ))}
       </div>
